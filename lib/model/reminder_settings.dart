15ae4c933cb3d0925a9f80794792d6650256e6ec
cagyei-quantum <cagyei@quantumgroupgh.com>
2026-04-14 11:35:04 +0000

    feat: Refactor ReminderSettings to store time as direct hour and minute

--- lib/model/reminder_settings.dart
+++ lib/model/reminder_settings.dart
@@ -1,33 +1,30 @@
+import 'package:flutter/material.dart';
+
 import '../core/utils/utils.dart';
 
 class ReminderSettings {
   const ReminderSettings({
     this.selectedPeriodIndex = 0,
-    this.selectedHourIndex = 6,
-    this.selectedMinuteIndex = 0,
-    this.selectedMeridiemIndex = 0,
+    this.hour = 7,
+    this.minute = 0,
     this.remindBefore = true,
   });
 
   final int selectedPeriodIndex;
-  final int selectedHourIndex;
-  final int selectedMinuteIndex;
-  final int selectedMeridiemIndex;
+  final int hour;
+  final int minute;
   final bool remindBefore;
 
   ReminderSettings copyWith({
     int? selectedPeriodIndex,
-    int? selectedHourIndex,
-    int? selectedMinuteIndex,
-    int? selectedMeridiemIndex,
+    int? hour,
+    int? minute,
     bool? remindBefore,
   }) {
     return ReminderSettings(
       selectedPeriodIndex: selectedPeriodIndex ?? this.selectedPeriodIndex,
-      selectedHourIndex: selectedHourIndex ?? this.selectedHourIndex,
-      selectedMinuteIndex: selectedMinuteIndex ?? this.selectedMinuteIndex,
-      selectedMeridiemIndex:
-          selectedMeridiemIndex ?? this.selectedMeridiemIndex,
+      hour: hour ?? this.hour,
+      minute: minute ?? this.minute,
       remindBefore: remindBefore ?? this.remindBefore,
     );
   }
@@ -35,32 +32,48 @@ class ReminderSettings {
   Map<String, dynamic> toMap() {
     return {
       'selectedPeriodIndex': selectedPeriodIndex,
-      'selectedHourIndex': selectedHourIndex,
-      'selectedMinuteIndex': selectedMinuteIndex,
-      'selectedMeridiemIndex': selectedMeridiemIndex,
+      'hour': hour,
+      'minute': minute,
       'remindBefore': remindBefore,
     };
   }
 
   factory ReminderSettings.fromMap(Map<dynamic, dynamic> map) {
+    final int legacyHourIndex = map['selectedHourIndex'] as int? ?? 6;
+    final int legacyMinuteIndex = map['selectedMinuteIndex'] as int? ?? 0;
+    final int legacyMeridiemIndex = map['selectedMeridiemIndex'] as int? ?? 0;
+
     return ReminderSettings(
       selectedPeriodIndex: map['selectedPeriodIndex'] as int? ?? 0,
-      selectedHourIndex: map['selectedHourIndex'] as int? ?? 6,
-      selectedMinuteIndex: map['selectedMinuteIndex'] as int? ?? 0,
-      selectedMeridiemIndex: map['selectedMeridiemIndex'] as int? ?? 0,
+      hour: map['hour'] as int? ??
+          _to24Hour(
+            legacyHourIndex + 1,
+            legacyMeridiemIndex == 0,
+          ),
+      minute: map['minute'] as int? ?? int.parse(minutes[legacyMinuteIndex]),
       remindBefore: map['remindBefore'] as bool? ?? true,
     );
   }
 
   String get periodLabel => periods[selectedPeriodIndex];
 
-  String get timeLabel {
-    final String hour = '${selectedHourIndex + 1}';
-    final String minute = minutes[selectedMinuteIndex];
-    final String meridiem = selectedMeridiemIndex == 0 ? 'AM' : 'PM';
-    return '$hour:$minute $meridiem';
+  TimeOfDay get timeOfDay => TimeOfDay(hour: hour, minute: minute);
+
+  String formatTime(BuildContext context) {
+    return MaterialLocalizations.of(context).formatTimeOfDay(
+      timeOfDay,
+      alwaysUse24HourFormat: MediaQuery.of(context).alwaysUse24HourFormat,
+    );
   }
 
   String get reminderLabel =>
-      remindBefore ? '1 hour before' : 'At workout time';
+      remindBefore ? '1 hour before' : 'At packing time';
+
+  static int _to24Hour(int hour, bool isAm) {
+    if (isAm) {
+      return hour == 12 ? 0 : hour;
+    }
+
+    return hour == 12 ? 12 : hour + 12;
+  }
 }
